0a577110f70c554e93702e102fb9d311b4df883b
Jerome Anos <jerome.anos03@gmail.com>
2022-06-29 22:45:05 +0800

    order-summary-component

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        redHat: "'Red Hat Display', sans-serif",
+      },
+      colors: {
+        paleBlue: 'hsl(225, 100%, 94%)',
+        brightBlue: 'hsl(245, 75%, 52%)',
+        veryPaleBlue: 'hsl(225, 100%, 98%)',
+        desaturatedBlue: 'hsl(224, 23%, 55%)',
+        darkBlue: 'hsl(223, 47%, 23%)',
+      },
+    },
+  },
+  plugins: [],
+};
